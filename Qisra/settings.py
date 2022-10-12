0b33055b750080265dcdc97ce2d1c8ea1a8f7ee1
Leo <leo56029132@gmail.com>
2022-10-12 09:13:01 +0800

    change debug mode to True to run in heroku

--- Qisra/settings.py
+++ Qisra/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+f(#3&pcejhb#sh*e0)8nwb(c81$tsirfw-=bd7#w#9y0$ckz4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'qisrabeta2.herokuapp.com',
                  "127.0.0.1"]
